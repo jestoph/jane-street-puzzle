e6f3fa4f8d8010a6960f170ecfcbb8e30a68658b
Christopher Jessup <cjessup@atlassian.com>
2026-08-31 17:08:57 +1000

    canonicalising part7

--- part7_simplified.v
+++ part7_simplified.v
@@ -20,9 +20,9 @@ assign FROM_PART7C0  = ( ~ Wire_544  ) & Wire_548  ;
   );
 
 assign Wire_554  = ( ( Wire_548  | Wire_545  ) & Wire_543  ) ;
-assign Wire_549  = ~ ( ( ~ Wire_548  ) & Wire_545  ) ; // Syntax?
-assign Wire_545  = ~ ( I  & S  & Wire_544  & Wire_999  ) ;
-assign Wire_543  = Wire_544  | ( I  & S  & Wire_999  ) ;
+assign Wire_549  = ~ ( ( ~ Wire_548  ) & Wire_545  ) ;
+assign Wire_545  = ~ ( I  & Wire_544  & Wire_999  ) ;
+assign Wire_543  = Wire_544  | ( I  & Wire_999  ) ;
 assign Wire_999  = ~ ( FROM_PART21  | FROM_PART22  | FROM_PART23  | FROM_PART20  ) ;
 
 /************************************ FROM_PART7A0 ********************************/
@@ -45,9 +45,9 @@ assign FROM_PART7A0  = ( ~ Wire_43  ) & Wire_127  ;
   );
 
 assign Wire_144  = ( ( Wire_127  | Wire_75  ) & Wire_158  ) ;
-assign Wire_102  = ~ ( ( ~ Wire_127  ) & Wire_75  ) ; // Syntax?
-assign Wire_75  = ~ ( I  & S  & Wire_43  & Wire_518  ) ;
-assign Wire_158  = Wire_43  | ( I  & S  & Wire_518  ) ;
+assign Wire_102  = ~ ( ( ~ Wire_127  ) & Wire_75  ) ;
+assign Wire_75  = ~ ( I  & Wire_43  & Wire_518  ) ;
+assign Wire_158  = Wire_43  | ( I  & Wire_518  ) ;
 assign Wire_518  = ( ~ FROM_PART22  & ~ FROM_PART20  ) & FROM_PART23  & FROM_PART21  ;
 
 /************************************ FROM_PART7A1 ********************************/
@@ -75,13 +75,11 @@ assign Wire_36  = Wire_64  | ( Wire_53  & Wire_516  ) ;
   // ref component/o21a.v
 assign Wire_521  = ( ( Wire_53  | Wire_516  ) & Wire_4  ) ;
   // ref component/nor2.v
-assign Wire_516  = ~ ( Wire_514  | Wire_515  ) ; // Syntax?
+assign Wire_516  = ~ ( Wire_514  | (~I)  ) ;
   // ref component/or4.v
-assign Wire_4  = Wire_64  | (~Wire_53)  | Wire_514  | Wire_515  ;
+assign Wire_4  = Wire_64  | (~Wire_53)  | Wire_514  | (~I)  ;
 // ref component/or4b.v
 assign Wire_514  = FROM_PART21  | FROM_PART22  | FROM_PART20  | ( ~ FROM_PART23  ) ;
-  // ref component/nand2.v
-assign Wire_515  = ~ ( I  & S  ) ; // Syntax?
 
 /************************************ FROM_PART7A2 ********************************/
 
@@ -106,11 +104,11 @@ assign FROM_PART7A2  = ( ~ Wire_517  ) & Wire_25  ;
   // ref component/o21a.v
 assign Wire_86  = ( ( Wire_25  | Wire_17  ) & Wire_114  ) ;
   // ref component/nand2b.v
-assign Wire_519  = ~ ( ( ~ Wire_25  ) & Wire_17  ) ; // Syntax?
+assign Wire_519  = ~ ( ( ~ Wire_25  ) & Wire_17  ) ;
   // ref component/nand4.v
-assign Wire_17  = ~ ( I  & S  & Wire_517  & Wire_407  ) ;
+assign Wire_17  = ~ ( I  & Wire_517  & Wire_407  ) ;
   // ref component/a31o.v
-assign Wire_114  = Wire_517  | ( I  & S  & Wire_407  ) ;
+assign Wire_114  = Wire_517  | ( I  & Wire_407  ) ;
 // ref component/and4bb.v
 assign Wire_407  = ( ~ FROM_PART21  & ~ FROM_PART20  ) & FROM_PART23  & FROM_PART22  ;
 
@@ -139,11 +137,9 @@ assign Wire_538  = Wire_436  | ( Wire_552  & Wire_542  ) ;
   // ref component/o21a.v
 assign Wire_409  = ( ( Wire_552  | Wire_542  ) & Wire_410  ) ;
   // ref component/or4.v
-assign Wire_410  = Wire_436  | (~Wire_552)  | Wire_540  | Wire_541  ;
+assign Wire_410  = Wire_436  | (~Wire_552)  | Wire_540  | (~I)  ;
   // ref component/nor2.v
-assign Wire_542  = ~ ( Wire_540  | Wire_541  ) ; // Syntax?
-  // ref component/nand2.v
-assign Wire_541  = ~ ( I  & S  ) ; // Syntax?
+assign Wire_542  = ~ ( Wire_540  | (~I)  ) ;
 // ref component/or4b.v
 assign Wire_540  = FROM_PART22  | FROM_PART23  | FROM_PART20  | ( ~ FROM_PART21  ) ;
 
@@ -172,13 +168,11 @@ assign Wire_547  = Wire_551  | ( Wire_408  & Wire_537  ) ;
   // ref component/o21a.v
 assign Wire_546  = ( ( Wire_408  | Wire_537  ) & Wire_707  ) ;
   // ref component/nor2.v
-assign Wire_537  = ~ ( Wire_550  | Wire_553  ) ; // Syntax?
+assign Wire_537  = ~ ( Wire_550  | (~I)  ) ;
   // ref component/or4.v
-assign Wire_707  = Wire_551  | (~Wire_408)  | Wire_550  | Wire_553  ;
+assign Wire_707  = Wire_551  | (~Wire_408)  | Wire_550  | (~I)  ;
 // ref component/or4b.v
 assign Wire_550  = FROM_PART21  | FROM_PART23  | FROM_PART20  | ( ~ FROM_PART22  ) ;
-  // ref component/nand2.v
-assign Wire_553  = ~ ( I  & S  ) ; // Syntax?
 
 /************************************ FROM_PART7B3 ********************************/
 
@@ -203,11 +197,11 @@ assign FROM_PART7B3  = ( ~ Wire_280  ) & Wire_197  ;
   // ref component/o21a.v
 assign Wire_200  = ( ( Wire_197  | Wire_195  ) & Wire_189  ) ;
   // ref component/nand2b.v
-assign Wire_313  = ~ ( ( ~ Wire_197  ) & Wire_195  ) ; // Syntax?
+assign Wire_313  = ~ ( ( ~ Wire_197  ) & Wire_195  ) ;
   // ref component/nand4.v
-assign Wire_195  = ~ ( I  & S  & Wire_280  & Wire_321  ) ;
+assign Wire_195  = ~ ( I & Wire_280  & Wire_321  ) ;
   // ref component/a31o.v
-assign Wire_189  = Wire_280  | ( I  & S  & Wire_321  ) ;
+assign Wire_189  = Wire_280  | ( I  & Wire_321  ) ;
 // ref component/and4b.v
 assign Wire_321  = ( ~ FROM_PART23  ) & FROM_PART20  & FROM_PART22  & FROM_PART21  ;
 
@@ -234,11 +228,11 @@ assign FROM_PART7B4  = ( ~ Wire_511  ) & Wire_482  ;
   // ref component/o21a.v
 assign Wire_534  = ( ( Wire_482  | Wire_524  ) & Wire_523  ) ;
   // ref component/nand2b.v
-assign Wire_520  = ~ ( ( ~ Wire_482  ) & Wire_524  ) ; // Syntax?
+assign Wire_520  = ~ ( ( ~ Wire_482  ) & Wire_524  ) ;
   // ref component/nand4.v
-assign Wire_524  = ~ ( I  & S  & Wire_511  & Wire_508  ) ;
+assign Wire_524  = ~ ( I  & Wire_511  & Wire_508  ) ;
   // ref component/a31o.v
-assign Wire_523  = Wire_511  | ( I  & S  & Wire_508  ) ;
+assign Wire_523  = Wire_511  | ( I  & Wire_508  ) ;
 // ref component/and4bb.v
 assign Wire_508  = ( ~ FROM_PART21  & ~ FROM_PART23  ) & FROM_PART20  & FROM_PART22  ;
 
@@ -265,11 +259,11 @@ assign FROM_PART7B5  = ( ~ Wire_227  ) & Wire_360  ;
   // ref component/o21a.v
 assign Wire_381  = ( ( Wire_360  | Wire_345  ) & Wire_433  ) ;
   // ref component/nand2b.v
-assign Wire_525  = ~ ( ( ~ Wire_360  ) & Wire_345  ) ; // Syntax?
+assign Wire_525  = ~ ( ( ~ Wire_360  ) & Wire_345  ) ;
   // ref component/nand4.v
-assign Wire_345  = ~ ( I  & S  & Wire_227  & Wire_377  ) ;
+assign Wire_345  = ~ ( I  & Wire_227  & Wire_377  ) ;
   // ref component/a31o.v
-assign Wire_433  = Wire_227  | ( I  & S  & Wire_377  ) ;
+assign Wire_433  = Wire_227  | ( I  & Wire_377  ) ;
 // ref component/and4bb.v
 assign Wire_377  = ( ~ FROM_PART22  & ~ FROM_PART23  ) & FROM_PART20  & FROM_PART21  ;
 
@@ -296,11 +290,11 @@ assign FROM_PART7B6  = ( ~ Wire_532  ) & Wire_452  ;
   // ref component/o21a.v
 assign Wire_526  = ( ( Wire_452  | Wire_529  ) & Wire_528  ) ;
   // ref component/nand2b.v
-assign Wire_464  = ~ ( ( ~ Wire_452  ) & Wire_529  ) ; // Syntax?
+assign Wire_464  = ~ ( ( ~ Wire_452  ) & Wire_529  ) ;
   // ref component/nand4.v
-assign Wire_529  = ~ ( I  & S  & Wire_532  & Wire_527  ) ;
+assign Wire_529  = ~ ( I  & Wire_532  & Wire_527  ) ;
   // ref component/a31o.v
-assign Wire_528  = Wire_532  | ( I  & S  & Wire_527  ) ;
+assign Wire_528  = Wire_532  | ( I  & Wire_527  ) ;
 // ref component/and4bb.v
 assign Wire_527  = ( ~ FROM_PART23  & ~ FROM_PART20  ) & FROM_PART22  & FROM_PART21  ;
 
@@ -329,10 +323,8 @@ assign Wire_531  = Wire_445  | ( Wire_454  & Wire_455  ) ;
   // ref component/o21a.v
 assign Wire_530  = ( ( Wire_454  | Wire_455  ) & Wire_533  ) ;
   // ref component/or4.v
-assign Wire_533  = Wire_445  | (~Wire_454)  | Wire_492  | Wire_506  ;
+assign Wire_533  = Wire_445  | (~Wire_454)  | Wire_492  | (~I)  ;
   // ref component/nor2.v
-assign Wire_455  = ~ ( Wire_492  | Wire_506  ) ; // Syntax?
+assign Wire_455  = ~ ( Wire_492  | (~I)  ) ;
 // ref component/or4b.v
 assign Wire_492  = FROM_PART21  | FROM_PART22  | FROM_PART23  | ( ~ FROM_PART20  ) ;
-  // ref component/nand2.v
-assign Wire_506  = ~ ( I  & S  ) ; // Syntax?
